14cc41581150714c4240b4111fafa2d1ef1375a4
joao-pampanin <joao.pampanin@pm.me>
2025-11-02 21:31:53 +0000

    add get_finished_tasks function

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import pandas as pd
 import json
 from helper.get_projects import get_projects
+from helper.get_finished_tasks import get_finished_tasks
 
 path = 'file.json'
 
 df = get_projects(path)
 df
+
+finished_df = get_finished_tasks(path)
+finished_df.sample(1).T
